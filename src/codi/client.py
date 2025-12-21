4771da683e7d94180fbdf285e0123c1465bb439d
matthew <matthewdelannoy527@gmail.com>
2025-12-21 21:55:28 +0100

    added status to states and gripper command to commands

--- src/codi/client.py
+++ src/codi/client.py
@@ -1,6 +1,6 @@
-"""CoDI (Colossus Desktop Interface)
+"""CoDI (Cora Desktop Interface)
 
-CoDI is an SDK developed by ... to interface with the open source Colossus cobot.
+CoDI is an SDK developed by ... to interface with the open source Cora cobot.
     https://github.com/machine0herald/CoDI
 ;-)
 """
@@ -16,7 +16,7 @@ import matplotlib
 import utils as ut
 import exeptions
 
-class ColossusClient:
+class CoraClient:
     def __init__(self, **kwargs):
         self._running = False
         self.arm_host = kwargs.get("host")
@@ -25,6 +25,7 @@ class ColossusClient:
         self.states_port = kwargs.get('states_port')
         self.receive_video_bool = kwargs.get('receive_video', True)
         self.receive_states_bool = kwargs.get('receive_states', True)
+        self.move_status = 'idle'  # 'idle', 'moving', 'at_target', 'error'
 
         try:
             self.arm_host = socket.gethostbyname(self.arm_host)
@@ -47,7 +48,7 @@ class ColossusClient:
             self.states_socket.connect((self.arm_host, self.states_port))
         
         except socket.error as e:
-            raise ConnectionError(f"Failed to connect to Colossus arm: {e}")
+            raise ConnectionError(f"Failed to connect to Cora arm: {e}")
         
         self.configure_sockets()
         
@@ -68,7 +69,7 @@ class ColossusClient:
             print("Client is already stopped.")
             return
         
-        print("Stopping Colossus client...")        
+        print("Stopping Cora client...")        
         self._running = False
         
         for sock in (self.command_socket, self.video_socket, self.states_socket):
@@ -97,24 +98,24 @@ class ColossusClient:
         """
         While socket is connected to server it listens 
         over Ethernet TCP socket for state information 
-        and stores it as self.states in the colossus client object.
+        and stores it as self.states in the Cora client object.
         """
         
         while self._running:
             if not self._running:
                 break            
             raw_states = self.states_socket.recv(1024)
-            self.state_space, self.states = ut.decode_pose_feedback(raw_states)
+            self.move_status,self.state_space, self.states = ut.decode_pose_feedback(raw_states)
 
-    
+
     def get_states(self, print=True):
         """
-        Get self.states from colossus client object.
+        Get self.states from Cora client object.
         """
         print(self.states)
         return self.states
-    
-    def send_command(self, command, space, rt, interface_type, print=True):
+
+    async def send_command(self, command, space, rt, interface_type, print=True, *args):
         """
         Takes the command array and descriptive information, \n
         encodes it into json format and sends it as raw bytes \n
@@ -129,25 +130,31 @@ class ColossusClient:
                         or [vx, vy, vz, wx, wy, wz] for velocity \n
                         or [Fx, Fy, Fz, Mx, My, Mz] for effort \n
 
-        :param rt: True for real-time, False for non-real-time
+        :param rt: True for real-time, False for non-real-time\n
 
-        :param interface_type: command interface type; 'position', 'velocity', 'effort'
+        :param interface_type: command interface type; 'position', 'velocity', 'effort'\n
+        
+        :param print: If True, prints the command being sent.\n
+
+        :param args: Additional arguments, e.g., gripper commands.
         """
-        if print == True:
-            print(f"Sending {interface_type} command to Colossus, {command} in {space} space.")
+        if print:
+            print(f"Sending {interface_type} command to Cora, {command} in {space} space.")
 
-        command_string = ut.encode_commands(command, space, rt, interface_type)
+        gripper_command = args[0] if args else np.array(0.0)  # get gripper command from args
+
+        command_string = ut.encode_commands(command, gripper_command, space, rt, interface_type)
         try:
             self.command_socket.sendall(command_string.encode('utf-8'))
         except socket.error as e:
-            raise ConnectionError(f"Failed to send command to Colossus arm: {e}")
+            raise ConnectionError(f"Failed to send command to Cora arm: {e}")
         return
     
-class GuiClient(ColossusClient):
+class GuiClient(CoraClient):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.root = tk.TK()
-        self.root.title('Colossus Control Panel')
+        self.root.title('Cora Control Panel')
 
         self._build_layout()
         self._update_states_loop()
--- src/codi/utils.py
+++ src/codi/utils.py
@@ -4,17 +4,20 @@ import json as js
 def bytes_to_image(byte_data, width, height):
       return 
 
-def encode_commands(commands, space, rt, interface_type):
+def encode_commands(commands, gripper_commands, space, rt, interface_type):
         """
         Serialize  goal pose command and gripper command into string format.
 
         :param space: 'JS' for joint space or 'TS' for task (cartesian) space
 
-        :param commands: numpy array of the values wrt the selected space;
+        :param commands: 1D numpy array of the values wrt the selected space;
                 format: 
                         [x, y, z, rx, ry, rz] for position 
                         or [vx, vy, vz, wx, wy, wz] for velocity
                         or [Fx, Fy, Fz, Mx, My, Mz] for effort
+
+        :param gripper_commands: 1D numpy array of the gripper commands
+                                format: [gripper_position] <between 0.0 (open) and 1.0 (closed)>
 
         :param rt: True for real-time, False for non-real-time
 
@@ -22,7 +25,9 @@ def encode_commands(commands, space, rt, interface_type):
 
         returns: UTF-8 encoded json formatted commands;
                 format: b'{'space': space, 'rt': rt, 'interface_type':interface_type, 'shape': [rows, columns],
-                                'type': dtype, 'data_array': [x, y, z, rx, ry, rz]}'
+                                'type': dtype, 'data_array': [x, y, z, rx, ry, rz]}'        
+        
+
         """
         command_data={
                'space': space,
@@ -30,7 +35,8 @@ def encode_commands(commands, space, rt, interface_type):
                'interface_type': interface_type,               
                'shape': list(commands.shape),
                'dtype': str(commands.dtype),
-               'data': commands.tolist()
+               'data': commands.tolist(),
+               'gripper_data': gripper_commands.tolist()
         }
 
         raw_json_commands = (js.dumps(command_data)).encode('utf-8')
@@ -38,16 +44,29 @@ def encode_commands(commands, space, rt, interface_type):
 
 def decode_pose_feedback(raw_json_states):
         """
-        Load pose with json, convert from list to numpy array
+        Load pose with json, convert from list to numpy array\n
 
-        :param raw_json_states: states in json format as raw bytes
-                format: b'{'space': space, 'shape': [rows, columns], 'type': dtype, 'data_array': [[x, y, z, rx, ry, rz], [vx, vy, vz, wx, wy, wz]]}'
+        :param raw_json_states: states in json format as raw bytes\n
+                format: b'{\n
+                'space': space,\n
+                'status': status,\n 
+                'shape': [rows, columns],\n 
+                'type': dtype, \n
+                'data_array': [[x, y, z, rx, ry, rz], [vx, vy, vz, wx, wy, wz]]\n
+                }'\n
 
-        returns [space, states_array] of type [string, numpy_array]
+        returns [status, space, states_array] of type [string, string, numpy_array];
+                status: 'idle', 'at_target', 'moving', 'error'\n
+                space: 'JS' for joint space or 'TS' for task (cartesian) space\n
+                states_array: numpy array of the states values;
+                        format:\n
+                                [[x, y, z, rx, ry, rz],\n
+                                 [vx, vy, vz, wx, wy, wz]]\n
 
         """
         json_states = js.loads(raw_json_states.decode('utf-8'))
         states_array = np.array([json_states["data"]], float)
         space = json_states["space"]
-        
-        return space, states_array
+        status = json_states["status"]
+
+        return status, space, states_array
