cc51c3c5672d51d77d9b547b68156237e20a528a
machine0herald <M.J.deLannoy@student.tudelft.nl>
2025-12-27 23:21:50 +0100

    thread handling

--- src/codi/client.py
+++ src/codi/client.py
@@ -16,30 +16,166 @@ import matplotlib
 from . import utils as ut
 from . import exeptions
 
-class CoraClient:
+class CoraInterface:
+
+    # TODO: Make it so that a program can be exited without killing the socket threads. 
+    # i.e. ready to use client objects that are always in connection with the server once started.
+    # this way other standard programs can also be included in the sdk, 
+    # like a controller interface for use with bluetooth controllers
+
+    # TODO: Add initialization step at startup that configures what 
+    # optional nodes need to be launched on the robot. 
+    # i.e. vision and bt controller nodes. this way when a program is terminated, 
+    # these optional nodes are killed and possibly (re)started on another program's startup.
+
     def __init__(self, **kwargs):
         self._running = False
-        self.arm_host = kwargs.get("host")
+        self.host = kwargs.get("host")
         self.video_port = kwargs.get('video_port')
         self.command_port = kwargs.get('command_port')
         self.states_port = kwargs.get('states_port')
-        self.receive_video_bool = kwargs.get('receive_video', True)
-        self.receive_states_bool = kwargs.get('receive_states', True)
-        self.move_status = 'idle'  # 'idle', 'moving', 'at_target', 'error'
-
-        try:
-            self.arm_host = socket.gethostbyname(self.arm_host)
-        except socket.gaierror:
-            raise ValueError(f"Could not resolve hostname: {self.arm_host}")
-        
+        self.config_port = kwargs.get('config_port')
+        self.move_status = 'idle'  # 'idle', 'moving', 'at_target', 'error', 'homing'
         self.video_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        # TCP socket for video
         self.command_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)     # TCP socket for commands
         self.states_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       # TCP socket for states
+        self.configuration_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP socket for configurations
+        self.extra_threads = dict()
 
+        try:
+            self.arm_host = socket.gethostbyname(self.arm_host)
+
+        except socket.gaierror:
+            raise ValueError(f"Could not resolve hostname: {self.arm_host}")
+        
+    def _init_extra_threads(self):
+        self.extra_threads = {
+            "video": {
+                "target": self.video,
+                "thread": None,
+                "stop_event": threading.Event(),
+                "active": False,
+                "daemon": True
+            }
+        }
+
+    def _start_thread(self, name):
+        entry = self.extra_threads[name]
+
+        if entry["thread"] and entry["thread"].is_alive():
+            return  # already running
+
+        entry["stop_event"].clear()
+
+        t = threading.Thread(
+            target=entry["target"],
+            daemon=entry["daemon"],
+            name=name
+        )
+
+        entry["thread"] = t
+        t.start()
+    
+    def _stop_thread(self, name):
+        entry = self.extra_threads[name]
+
+        if not entry["thread"]:
+            return
+
+        entry["stop_event"].set()
+
+        if entry["thread"].is_alive():
+            entry["thread"].join(timeout=1.0)
+
+        entry["thread"] = None
+
+    def _reconcile_threads(self, active_set):
+        """
+        active_set: iterable of thread names that SHOULD be running
+        """
+
+        for name, entry in self.extra_threads.items():
+            should_run = name in active_set
+
+            if should_run and not entry["active"]:
+                self._start_thread(name)
+                entry["active"] = True
+
+            elif not should_run and entry["active"]:
+                self._stop_thread(name)
+                entry["active"] = False
+    
+    def _connect(self):
+        try:
+            self.command_socket.connect((self.host, self.command_port))
+            self.video_socket.connect((self.host, self.video_port))
+            self.states_socket.connect((self.host, self.states_port))
+            self.configuration_socket.connect((self.host, self.config_port))
+        
+        except socket.error as e:
+            raise ConnectionError(f"Failed to connect to Cora arm: {e}")
+        
+        self.configure_sockets()
+        
+        self._running = True
+
+        # Start the states Thread
+        self.states_thread = threading.Thread(target=self.states, daemon=True)
+
+        return
+    
+    def _kill(self):
+        if self._running is False:
+            print("Client is already stopped.")
+            return
+        
+        print("Stopping Cora client...")        
+        self._running = False
+        
+        for sock in (self.command_socket, self.video_socket, self.states_socket):
+            if sock:
+                try:
+                    sock.shutdown(socket.SHUT_RDWR)
+                    sock.close()
+                except OSError:
+                    pass
+
+        return
+
+
+class CoraClient(CoraInterface):
+    def __init__(self, **kwargs):
+        self.use_camera = False
+        self.use_controller = False
+        self.use_vision = False
+
+        super().__init__(self, **kwargs)
+        
     def configure_sockets(self):
         self.video_socket.shutdown(socket.SHUT_WR)
         self.states_socket.shutdown(socket.SHUT_WR)
         self.command_socket.shutdown(socket.SHUT_RD)
+
+    def configure_robot(self, **kwargs):
+        self.use_controller = kwargs.get("use_controller")
+        self.use_camera = kwargs.get("use_camera")
+        self.use_vision = kwargs.get("use_vision")
+
+    def kill_options(self):
+        self.use_controller = False
+        self.use_camera = False
+        self.use_vision = False
+    
+    def update_options(self):
+        active = set()
+
+        if self.use_camera:
+            active.add("video")
+
+        if True:  # always listen to states
+            active.add("states")
+
+        self.reconcile_threads(active)
 
     def  connect(self):
         try:
@@ -54,14 +190,6 @@ class CoraClient:
         
         self._running = True
         self._start_threads()
-        return
-
-    def _start_threads(self):
-        self.video_thread = threading.Thread(target=self.receive_video, daemon=True)
-        self.states_thread = threading.Thread(target=self.receive_states, daemon=True)
-
-        self.video_thread.start()
-        self.states_thread.start()
         return
 
     def kill(self):
@@ -150,13 +278,20 @@ class CoraClient:
             raise ConnectionError(f"Failed to send command to Cora arm: {e}")
         return
     
-class GuiClient(CoraClient):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.root = tk.TK()
-        self.root.title('Cora Control Panel')
+class CoraServer(CoraInterface):
 
-        self._build_layout()
-        self._update_states_loop()
+    # TODO: Implement cora server derived class with appropriate methods
+
+    def __init__(self, **kwargs):
+        super().__init__(self, **kwargs)
+        
+# class GuiClient(CoraClient):
+#     def __init__(self, **kwargs):
+#         super().__init__(**kwargs)
+#         self.root = tk.TK()
+#         self.root.title('Cora Control Panel')
+
+#         self._build_layout()
+#         self._update_states_loop()
 
         
