e943f52957d7cc5cd7120378f3346f2e7a8fd7bb
matthew <matthewdelannoy527@gmail.com>
2026-06-13 04:08:49 +0200

    push sphinx docs

---
+++ docs/source/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'CORA'
+copyright = '2026, Matthew de Lannoy'
+author = 'Matthew de Lannoy'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))  # adjust to point at your package root
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',       # if you ever use Google/NumPy style docstrings
+    'sphinx_autodoc_typehints',  # optional, renders type hints nicely
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
